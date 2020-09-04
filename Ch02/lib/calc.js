0fa8e3311150a3d882f9e8746fa4515ec403e102
sooho0163 <sooh0163o@naver.com>
2020-09-04 12:27:09 +0900

    fff

---
+++ Ch02/lib/calc.js
@@ -0,0 +1,19 @@
+function plus(x,y){
+    var z = x + y;
+    return z;
+}
+
+function minus(x,y){
+    var z = x - y;
+    return z;
+}
+
+function multi(x,y){
+    var z = x * y;
+    return z;
+}
+
+function div(x,y){
+    var z = x / y;
+    return z;
+}
